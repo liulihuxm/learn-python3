112e5a4a7c0fa3b4848620048f3c04681eefc034
liulihuxm <57760032+liulihuxm@users.noreply.github.com>
2019-11-14 22:00:58 +0800

    Update do_filter.py

--- samples/functional/do_filter.py
+++ samples/functional/do_filter.py
@@ -12,3 +12,26 @@ def not_empty(s):
     return s and s.strip()
 
 print(list(filter(not_empty, ['A', '', 'B', None, 'C', '  '])))
+
+
+from functools import reduce
+def is_palindrome(n):
+    L=[]
+    N=n
+    while True:
+        L.append(N%10)
+        if N//10==0:
+            break
+        else:
+            N=N//10
+    output=reduce(lambda x,y:x*10+y,L)
+    return output==n
+
+
+
+
+def is_palindrome(n):
+	return str(n)==str(n)[::-1]
+ 
+output = filter(is_palindrome, range(1, 1000))
+print(list(output))
